70b796aa759d26eaef5b7fb994a2e3ce04fdffbb
WaiNaat <qoli0703@gmail.com>
2022-10-01 18:00:09 +0900

    Update geturl.py

--- geturl.py
+++ geturl.py
@@ -4,6 +4,8 @@ from https://github.com/chenyaofo/pytorch-cifar-models/releases/
 '''
 
 url = {
+    'cifar10_vgg11_bn': 'https://github.com/chenyaofo/pytorch-cifar-models/releases/download/vgg/cifar10_vgg11_bn-eaeebf42.pt',
+    'cifar100_vgg11_bn': 'https://github.com/chenyaofo/pytorch-cifar-models/releases/download/vgg/cifar100_vgg11_bn-57d0759e.pt',
     'cifar10_vgg19_bn': 'https://github.com/chenyaofo/pytorch-cifar-models/releases/download/vgg/cifar10_vgg19_bn-57191229.pt',
     'cifar100_vgg19_bn': 'https://github.com/chenyaofo/pytorch-cifar-models/releases/download/vgg/cifar100_vgg19_bn-b98f7bd7.pt'
 }
